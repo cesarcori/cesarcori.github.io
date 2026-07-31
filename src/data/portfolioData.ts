1190ecd7f9c6349ece7852e7cda1ff6058344ab4
cesarcori <jcesarcori@gmail.com>
2026-07-31 17:24:22 -0400

    feat: update new project

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -1,14 +1,22 @@
 // 1. Projects Data
 export const PROJECTS = [
     {
+        badge: "Flashcards",
+        description: "An interactive learning application that uses dynamic flashcard decks to help users master any topic",
+        url: "https://github.com/cesarcori/angular-flashcards",
+        demo: "https://cesarcori.github.io/angular-flashcards/",
+    },
+    {
         badge: "System to manage university projects degree",
         description: "This project was developed with Django, Python and Bootstrap.",
         url: "https://github.com/cesarcori/etn1040_1",
+        demo: ""
     },
     {
         badge: "Instagram clone",
         description: "I build a clone of instagram with laravel 9.",
         url: "https://github.com/cesarcori/instagram-clone-laravel",
+        demo: ""
     },
 ] as const;
 
@@ -23,13 +31,16 @@ export const SKILLS = {
         "Vue.js",
         "Quasar.js",
         "Astro",
-        "TailwindCSS"
+        "TailwindCSS",
+        "Angular",
+        "MaterialUI"
     ],
     backend: [
         "Node.js",
         "Express",
         "Python",
         "Flask",
+        "FastAPI",
         "Java",
         "Django",
         "Laravel"
@@ -41,6 +52,8 @@ export const SKILLS = {
         "PostgreSQL",
         "SQLite",
         "Postman",
+        "AWS",
+        "Terraform",
         "Linux (Arch/Fedora/Ubuntu/Debian)"
     ]
 } as const;
